26669c9b2973487ced5e06098b0cd58bc4cfa38c
ysmolyar <yonismolyar1@gmail.com>
2018-08-18 11:19:41 -0400

    simple commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FriendPreviewComponent } from './components/friend-preview/friend-previ
 import { RecipeReviewComponent } from './components/recipe-review/recipe-review.component';
 import { SearchResultsPageComponent } from './pages/search-results-page/search-results-page.component';
 import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
+import { RandomRecipeComponent } from './components/random-recipe/random-recipe.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
     FriendPreviewComponent,
     RecipeReviewComponent,
     SearchResultsPageComponent,
-    RecipePageComponent
+    RecipePageComponent,
+    RandomRecipeComponent
   ],
   imports: [
     BrowserModule,
