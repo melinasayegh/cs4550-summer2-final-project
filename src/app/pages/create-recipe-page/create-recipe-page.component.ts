36ec10e6134bbb7bf88ec17d37fb85edbe723020
Melina <sayeghmelina@gmail.com>
2018-08-19 19:14:34 -0400

    working on update recipe

--- src/app/pages/create-recipe-page/create-recipe-page.component.ts
+++ src/app/pages/create-recipe-page/create-recipe-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {RecipeServiceClient} from '../../services/recipe.service.client';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {UserServiceClient} from '../../services/user.service.client';
 
 @Component({
@@ -10,33 +10,82 @@ import {UserServiceClient} from '../../services/user.service.client';
 })
 export class CreateRecipePageComponent implements OnInit {
 
+    @Input() recipe = {
+        title: String,
+        image: String,
+        description: String,
+        ingredients: String,
+        directions: String,
+        prepTime: String,
+        cookTime: String,
+        numServings: String,
+        tags: [String],
+        directionsList: [],
+        ingredientsList: []
+    }
     user = {
         _id: String
     };
-  title = '';
-  image = '';
-  description = '';
-  ingredients = '';
-  directions = '';
-  prepTime = '';
-  cookTime = '';
-  numServings = '';
-  tags: [String];
-  directionsList: string[];
-  ingredientsList: string[];
+  title;
+  image;
+  description;
+  ingredients;
+  directions;
+  prepTime;
+  cookTime;
+  numServings ;
+  tags;
+  directionsList;
+  ingredientsList;
 
   constructor(private router: Router,
+              private route: ActivatedRoute,
               private userService: UserServiceClient,
-              private recipeService: RecipeServiceClient) { }
+              private recipeService: RecipeServiceClient) {
 
-  createRecipe() {
+  }
 
+  setFields = () => {
+      this.title = this.recipe.title;
+      this.description = this.recipe.description;
+      this.image = this.recipe.image;
+      this.ingredients = this.recipe.ingredientsList;
+      this.directions = this.recipe.directionsList;
+      this.prepTime = this.recipe.prepTime;
+      this.cookTime  = this.recipe.cookTime;
+      this.numServings  =  this.recipe.numServings;
+  }
+
+    updateRecipe = () => {
+
+        this.directionsList = this.directions.split('\n');
+        this.ingredientsList = this.ingredients.split('\n');
+
+        const recipe = {
+            title: this.title,
+            description: this.description,
+            image: this.image,
+            creator: this.user._id,
+            // created at doesnt change
+            updatedAt: new Date(),
+            ingredients: this.ingredientsList,
+            directions: this.directionsList,
+            prepTime: Number(this.prepTime),
+            cookTime: Number(this.cookTime),
+            numServings: Number(this.numServings),
+            tags: ['NONE'],
+        };
+        //this.recipeService.updateRecipe(id, recipe);
+    }
+
+  createRecipe = () => {
       this.directionsList = this.directions.split('\n');
       this.ingredientsList = this.ingredients.split('\n');
 
     const recipe = {
       title: this.title,
         description: this.description,
+        image: this.image,
         creator: this.user._id,
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -52,5 +101,6 @@ export class CreateRecipePageComponent implements OnInit {
 
   ngOnInit() {
       this.userService.profile().then(user => this.user = user);
+      //this.recipeService.findRecipeById()
   }
 }
