7fb648e3ef1cab0f170924242634704696b33c45
ysmolyar <yonismolyar1@gmail.com>
2018-08-22 02:12:00 -0400

    made api rendering from recipe page

--- src/app/services/recipe-api.service.client.ts
+++ src/app/services/recipe-api.service.client.ts
@@ -25,4 +25,13 @@ export class RecipeApiServiceClient {
             .then(response => response.json());
     }
 
+    findRandomRecipes = () => {
+        return fetch(SPOONACULAR_API + 'random?limitLicense=false&number=8', {
+            headers: {
+                'accept': 'application/json',
+                'x-mashape-key': 'VsYAEwDWxwmshX990l6hWa2WtVNAp1f1zBojsnIEiyKW9hG6Sf'
+            }
+        })
+            .then(response => response.json());
+    }
 }
